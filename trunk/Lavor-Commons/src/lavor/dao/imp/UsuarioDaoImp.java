9a600df50ca299b5c665f49142739bdb9f904a7e
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2009-12-29 21:22:33 +0000

    Adicionado suporte a usuario

---
+++ trunk/Lavor-Commons/src/lavor/dao/imp/UsuarioDaoImp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.dao.imp;
+
+import lavor.dao.UsuarioDao;
+import lavor.entidade.Usuario;
+
+/**
+ *
+ * @author marcelo
+ */
+public class UsuarioDaoImp  extends DaoGenericoImp<Usuario, Long> implements UsuarioDao{
+
+}
