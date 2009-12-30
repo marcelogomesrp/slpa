f5121e5c15439011359ca8ed8fe937acc9d14a8b
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2009-12-30 13:26:03 +0000

    Adicionado suporte a Peca

---
+++ Lavor-WebAdm/src/java/lavor/backbean/PecaBB.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.backbean;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import lavor.entidade.Peca;
+import lavor.managedbean.PecaMB;
+import lavor.service.PecaService;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author marcelo
+ */
+@Controller("pecaBB")
+@Scope("request")
+public class PecaBB {
+
+    @Resource
+    private PecaService pecaService;
+    @Resource
+    private PecaMB pecaMB;
+
+
+    public PecaService getPecaService() {
+        return pecaService;
+    }
+
+    public void setPecaService(PecaService pecaService) {
+        this.pecaService = pecaService;
+    }
+
+    public PecaMB getPecaMB() {
+        return pecaMB;
+    }
+
+    public void setPecaMB(PecaMB pecaMB) {
+        this.pecaMB = pecaMB;
+    }
+
+    public PecaBB() {
+    }
+
+
+    public String SalvarPeca(){
+        try {
+            pecaService.SalvarPeca(pecaMB.getPeca());
+            lavor.util.FacesUtils.mensInfo("Peca salva com sucesso");
+            pecaMB.setPeca(new Peca());
+        } catch (Exception ex) {
+            lavor.util.FacesUtils.mensErro("Erro ao salvar peca " + ex.getMessage());
+            Logger.getLogger(PecaBB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "sucesso";
+    }
+
+}
---
+++ Lavor-WebAdm/src/java/lavor/managedbean/PecaMB.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.managedbean;
+
+import javax.annotation.Resource;
+import lavor.entidade.Peca;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+
+/**
+ *
+ * @author marcelo
+ */
+
+@Controller("pecaMB")
+@Scope("session")
+public class PecaMB {
+    
+    private Peca peca;
+
+    public Peca getPeca() {
+        return peca;
+    }
+
+    public void setPeca(Peca peca) {
+        this.peca = peca;
+    }
+
+    public PecaMB() {
+        this.peca = new Peca();
+    }
+
+}
