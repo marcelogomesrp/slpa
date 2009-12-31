3b4f981d46e6d481d910a32126095053559d081a
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2009-12-31 19:04:34 +0000

    Salvando a lista de pecas do equipamento

--- trunk/Lavor-WebAdm/src/java/lavor/backbean/templateclient.java
+++ trunk/Lavor-WebAdm/src/java/lavor/backbean/templateclient.java
@@ -7,7 +7,17 @@ package lavor.backbean;
 
 
 
-import javax.swing.JOptionPane;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import lavor.entidade.Categoria;
+import lavor.entidade.Equipamento;
+import lavor.entidade.Peca;
+import lavor.service.EquipamentoService;
+import lavor.service.PecaService;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
@@ -19,15 +29,52 @@ import org.springframework.stereotype.Controller;
 @Controller("tt")
 @Scope("request")
 public class templateclient {
+    @Resource
+    EquipamentoService equipamentoService;
+    @Resource
+    PecaService pecaService;
 
     private String nome;
+
+    public EquipamentoService getEquipamentoService() {
+        return equipamentoService;
+    }
+
+    public void setEquipamentoService(EquipamentoService equipamentoService) {
+        this.equipamentoService = equipamentoService;
+    }
+
+
+
 
     public templateclient() {
         this.nome = "funcionou";
     }
 
     public String Valor(){
-        //JOptionPane.showMessageDialog(null, "to retornoando ok");
+        Equipamento equipamento = new Equipamento();
+        equipamento.setNome("equipamento teste");
+        equipamento.setCategoria(new Categoria("cat add auto", "descricao", Boolean.TRUE));
+        //Collection<Peca> pecas = new ArrayList<Peca>();
+
+//        for(int x = 0; x < 20; x++){
+//            Peca p = new Peca();
+//            p.setNome("peca " + x);
+//            //pecas.add(p);
+//            equipamento.getPecas().add(p);
+//        }
+        equipamento.setPecas(pecaService.BuscarPecaPorNome("%"));
+        
+
+        //equipamento.setPecas(pecas);
+        try {
+            equipamentoService.Salvar(equipamento);
+            //equipamentoService.Atualizar(equipamento);
+            this.nome="OK";
+        } catch (Exception ex) {
+            this.nome = "erro " + ex.getMessage();
+            Logger.getLogger(templateclient.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return "ok";
     }
 
