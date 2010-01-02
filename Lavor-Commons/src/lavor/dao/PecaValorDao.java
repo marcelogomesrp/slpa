66ffdbe977a7399083813c9c04f6d842620d9f27
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2010-01-02 14:27:53 +0000

    Adicionado suporte a lista de preco

---
+++ Lavor-Commons/src/lavor/dao/PecaValorDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.dao;
+
+import lavor.entidade.PecaValor;
+
+/**
+ *
+ * @author marcelo
+ */
+public interface PecaValorDao  extends DaoGenerico<PecaValor, Long>{
+
+}
---
+++ Lavor-Commons/src/lavor/dao/imp/ListaDePrecoDaoImp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.dao.imp;
+
+import lavor.dao.ListaDePrecoDao;
+import lavor.entidade.ListaDePreco;
+
+/**
+ *
+ * @author marcelo
+ */
+public class ListaDePrecoDaoImp extends DaoGenericoImp<ListaDePreco, Long> implements ListaDePrecoDao{
+
+}
