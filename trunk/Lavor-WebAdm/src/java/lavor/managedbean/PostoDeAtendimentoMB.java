910186c39856ef49c48b843341fd12d265993443
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2010-01-09 13:48:19 +0000

    Adicionado suporte ao posto de atendimento

---
+++ trunk/Lavor-WebAdm/src/java/lavor/managedbean/PostoDeAtendimentoMB.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.managedbean;
+
+import lavor.entidade.PostoDeAtendimento;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author marcelo
+ */
+
+@Controller("postoDeAtendimentoMB")
+@Scope("session")
+public class PostoDeAtendimentoMB {
+    
+    private PostoDeAtendimento postoDeAtendimento;
+
+    public PostoDeAtendimentoMB() {
+        this.postoDeAtendimento = new PostoDeAtendimento();
+    }
+
+    public PostoDeAtendimento getPostoDeAtendimento() {
+        return postoDeAtendimento;
+    }
+
+    public void setPostoDeAtendimento(PostoDeAtendimento postoDeAtendimento) {
+        this.postoDeAtendimento = postoDeAtendimento;
+    }
+
+
+}
