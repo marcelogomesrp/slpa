7ac2aff0572138c3275b5e5a471e36c15136e96a
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2010-03-01 21:47:49 +0000

    lavoe2

--- trunk/Lavor2-Web/src/java/lavor/backbean/PostoDeAtendimentoBB.java
+++ trunk/Lavor2-Web/src/java/lavor/backbean/PostoDeAtendimentoBB.java
@@ -5,8 +5,13 @@
 
 package lavor.backbean;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.Resource;
+import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
 import lavor.entidade.Cidade;
+import lavor.entidade.Estado;
 import lavor.entidade.PostoDeAtendimento;
 import lavor.entidade.TipoUsuario;
 import lavor.managedBean.CidadeMB;
@@ -39,9 +44,12 @@ public class PostoDeAtendimentoBB {
     private UsuarioService usuarioService;
     @Resource
     private CidadeService cidadeService;
+
+    private Boolean postosDeAtendimentoTemItem;
     
 
     public PostoDeAtendimentoBB() {
+        this.postosDeAtendimentoTemItem = false;
     }
 
     public PostoDeAtendimentoMB getPostoDeAtendimentoMB() {
@@ -52,15 +60,44 @@ public class PostoDeAtendimentoBB {
         this.postoDeAtendimentoMB = postoDeAtendimentoMB;
     }
 
+    public Boolean getPostosDeAtendimentoTemItem() {
+        return (postoDeAtendimentoMB.getPostosDeAtendimento().getRowCount() > 0? Boolean.TRUE:Boolean.FALSE);
+    }
+
+    public void setPostosDeAtendimentoTemItem(Boolean postosDeAtendimentoTemItem) {
+        this.postosDeAtendimentoTemItem = postosDeAtendimentoTemItem;
+    }
+
+
+
     public String DoNovoPage(){
         postoDeAtendimentoMB.setPostoDeAtendimento(new PostoDeAtendimento());
+        postoDeAtendimentoMB.setPostosDeAtendimento(new ListDataModel());
         return "/admin/posto/novo";
     }
 
     public String DoListarPage(){
         postoDeAtendimentoMB.setPostoDeAtendimento(new PostoDeAtendimento());
+        this.cidadeMB.setCidades(new ArrayList<SelectItem>());
+        this.postoDeAtendimentoMB.setPostosDeAtendimento(new ListDataModel());
         return "/admin/posto/listar";
     }
+
+    public String DoListarPageVoltar(){
+        return "/admin/posto/listar";
+    }
+
+    public String DoDetalhesPage(){
+        this.postoDeAtendimentoMB.setPostoDeAtendimento((PostoDeAtendimento) this.postoDeAtendimentoMB.getPostosDeAtendimento().getRowData());
+        return "posto/detalhes";
+    }
+
+    public String DoEditarPage(){
+        this.postoDeAtendimentoMB.setPostoDeAtendimento((PostoDeAtendimento) this.postoDeAtendimentoMB.getPostosDeAtendimento().getRowData());
+        return "posto/editar";
+    }
+
+    
 
     public String Salvar(){
         PostoDeAtendimento novoPostoDeAtendimento = postoDeAtendimentoMB.getPostoDeAtendimento();
@@ -77,29 +114,33 @@ public class PostoDeAtendimentoBB {
             postoDeAtendimentoMB.setPostoDeAtendimento(new PostoDeAtendimento());
         }catch(Exception ex){
             FacesUtils.adicionarMensagem("base_message", ex, "Ocorreu uma falha ao tentar salvar..");
+        }        
+        return "sucesso";
+    }
+
+    public String Atualizar(){
+        PostoDeAtendimento novoPostoDeAtendimento = postoDeAtendimentoMB.getPostoDeAtendimento();
+        Cidade cidade = novoPostoDeAtendimento.getCidade();
+        cidade = cidadeService.PesquisarPorCidadeEstado(cidade.getCidade(), cidade.getEstado());
+        novoPostoDeAtendimento.setCidade(cidade);
+        try{
+            //postoDeAtendimentoService.Atualizar(novoPostoDeAtendimento);
+            FacesUtils.adicionarMensagem("base_message", GenericExceptionMessageType.INFO, "Posto atualizado com sucesso" );
+            postoDeAtendimentoMB.setPostoDeAtendimento(new PostoDeAtendimento());
+        }catch(Exception ex){
+            FacesUtils.adicionarMensagem("base_message", ex, "Ocorreu uma falha ao tentar atualizar..");
         }
-        
-//        PostoDeAtendimento novoPostoDeAtendimento = postoDeAtendimentoMB.getPostoDeAtendimento();
-//        Usuario novoUsuario = novoPostoDeAtendimento.getUsuario();
-//        Cidade cidade = novoPostoDeAtendimento.getCidade();
-//        cidade = cidadeService.PesquisarPorCidadeEstado(cidade.getCidade(), cidade.getEstado());
-//        novoPostoDeAtendimento.setCidade(cidade);
-//        novoUsuario.setTipoUsuario(TipoUsuario.postoDeAtendimento);
-//        novoUsuario.setAtivo(Boolean.TRUE);
-//        try {
-//            if(novoUsuario.getId() == null){
-//                novoUsuario = usuarioService.Salvar(novoUsuario);
-//                novoPostoDeAtendimento.setUsuario(novoUsuario);
-//                novoPostoDeAtendimento = postoDeAtendimentoService.Salvar(novoPostoDeAtendimento);
-//                lavor.utils.FacesUtils_teste.mensInfo("Posto de atendimento salvo com sucesso ");
-//                postoDeAtendimentoMB.setPostoDeAtendimento(new PostoDeAtendimento());
-//                cidadeMB.LimparListaDeCidades();
-//            }
-//        } catch (Exception ex) {
-//                 FacesUtils.adicionarMensagem("base_message", ex, "Ocorreu uma falha ao tentar salvar..");
-//            //lavor.utils.FacesUtils_teste.mensErro("Ocorreu um erro ao tentar salvar. " + ex.getMessage() + " causa " + ex.getCause());
-//            Logger.getLogger(PostoDeAtendimentoBB.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        return "sucesso";
+    }
+
+
+    public String PesquisarPorCidade(){
+        Cidade cidade = this.postoDeAtendimentoMB.getPostoDeAtendimento().getCidade();
+        cidade = cidadeService.PesquisarPorCidadeEstado(cidade.getCidade(), cidade.getEstado());
+        List<PostoDeAtendimento> postos = this.postoDeAtendimentoService.pesquisarPorCidade(cidade.getId());
+        //List<PostoDeAtendimento> postos = this.postoDeAtendimentoService.pesquisarPorCidade(1L);
+        this.postoDeAtendimentoMB.setPostosDeAtendimento(new ListDataModel(postos));
+
         return "sucesso";
     }
 
