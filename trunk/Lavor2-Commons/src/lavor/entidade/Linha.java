9c3822d2f0309e288f8547465d49cd633b9c86d7
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2010-03-02 00:22:55 +0000

    linha

---
+++ trunk/Lavor2-Commons/src/lavor/entidade/Linha.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.entidade;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author marcelo
+ */
+@Entity
+@Table(name="linha")
+public class Linha implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=IDENTITY)
+    private Long id;
+    @Column(name="nome", length=30, unique=true, nullable=false)
+    private String nome;
+    @Lob
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Linha)) {
+            return false;
+        }
+        Linha other = (Linha) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "lavor.entidade.Linha[id=" + id + "]";
+    }
+
+}
