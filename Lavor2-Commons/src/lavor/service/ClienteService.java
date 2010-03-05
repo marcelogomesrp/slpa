42bdd9fa58c2f45fa797cf86601bf79bb002f3ae
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2010-03-05 00:56:30 +0000

    Cidade

---
+++ Lavor2-Commons/src/lavor/service/ClienteService.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.service;
+
+import java.io.Serializable;
+import lavor.dao.ClienteDao;
+import lavor.entidade.Cliente;
+import lavor.utils.GenericExceptionMessageType;
+
+/**
+ *
+ * @author marcelo
+ */
+public class ClienteService implements Serializable{
+    private ServiceException serviceException;
+    private ClienteDao clienteDao;
+
+    public ClienteService() {
+    }
+
+    public ClienteDao getClienteDao() {
+        return clienteDao;
+    }
+
+    public void setClienteDao(ClienteDao clienteDao) {
+        this.clienteDao = clienteDao;
+    }
+
+    private void NomeValido(String nome) {
+        if(lavor.utils.StringUtils.isNullOrEmpty(nome)){
+            serviceException.addMessage(GenericExceptionMessageType.WARNING, "O nome deve ser informado");
+        }
+    }
+
+    private boolean PodeSerSalvo(Cliente cliente) {
+        this.NomeValido(cliente.getNome());
+        return Boolean.TRUE;
+
+    }
+
+    public Cliente Salvar(Cliente cliente) throws ServiceException{
+        this.serviceException = new ServiceException();
+        try{
+            if(this.PodeSerSalvo(cliente)){
+                //cliente = this.clienteDao.salvar(cliente);
+                cliente = this.clienteDao.atualizar(cliente);
+            }
+        }catch(Exception ex){
+            throw new ServiceException("Ocorreu um erro ao tentar salvar", ex);
+        }
+        return cliente;
+    }
+
+
+
+
+
+}
