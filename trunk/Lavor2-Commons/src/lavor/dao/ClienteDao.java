3100887892146bd4727836a7bfe8d47fe4d89a2a
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2010-03-05 00:56:30 +0000

    Cidade

---
+++ trunk/Lavor2-Commons/src/lavor/dao/ClienteDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.dao;
+
+import lavor.entidade.Cliente;
+
+/**
+ *
+ * @author marcelo
+ */
+public interface ClienteDao extends DaoGenerico<Cliente, Long>{
+
+}
---
+++ trunk/Lavor2-Commons/src/lavor/entidade/Cliente.java
@@ -0,0 +1,136 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.entidade;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author marcelo
+ */
+@Entity
+@Table(name="cliente")
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=IDENTITY)
+    private Long id;
+    @Column(name="nome", length=255)
+    private String nome;
+    @Column(name="telefone", length=10)
+    private String telefone;
+    @Column(name="celular", length=10)
+    private String celular;
+    @Column(name="endereco", length=255)
+    private String endereco;
+    @OneToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+    @JoinColumn(name="id_cidade")
+    private Cidade cidade;
+    @OneToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+    @JoinColumn(name="id_posto_de_atendimento")
+    private PostoDeAtendimento postoDeAtendimento;
+
+    public Cliente() {
+        this.cidade             = new Cidade();
+        this.postoDeAtendimento = new PostoDeAtendimento();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular.replaceAll("[^\\d{L}]", "");
+        //this.celular = celular;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone.replaceAll("[^\\d{L}]", "");
+        //this.telefone = telefone;
+    }
+
+    public PostoDeAtendimento getPostoDeAtendimento() {
+        return postoDeAtendimento;
+    }
+
+    public void setPostoDeAtendimento(PostoDeAtendimento postoDeAtendimento) {
+        this.postoDeAtendimento = postoDeAtendimento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "lavor.entidade.Cliente[id=" + id + "]";
+    }
+
+}
