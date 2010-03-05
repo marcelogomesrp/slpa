d96224d886fc1fd19ce0e3a4dd0fa3a566e50733
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2010-03-05 00:56:52 +0000

    Cidade

---
+++ Lavor2-Web/src/java/lavor/backbean/ClienteBB.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.backbean;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import lavor.entidade.Cliente;
+import lavor.managedBean.ClienteMB;
+import lavor.managedBean.PostoDeAtendimentoMB;
+import lavor.service.CidadeService;
+import lavor.service.ClienteService;
+import lavor.service.ServiceException;
+import lavor.utils.FacesUtils;
+import lavor.utils.GenericExceptionMessageType;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author marcelo
+ */
+
+@Controller("clienteBB")
+@Scope("request")
+public class ClienteBB {
+    @Resource
+    private ClienteMB clienteMB;
+    @Resource
+    private PostoDeAtendimentoMB postoDeAtendimentoMB;
+
+    @Resource
+    private CidadeService cidadeService;
+    @Resource
+    private ClienteService clienteService;
+
+    public ClienteBB() {
+    }
+
+    public String DoNovoClientePage(){
+        this.clienteMB.setCliente(new Cliente());
+        return "sucesso";
+    }
+
+    public String Salvar(){
+        try {
+            Cliente cliente = clienteMB.getCliente();
+            cliente.setPostoDeAtendimento(this.postoDeAtendimentoMB.getPostoDeAtendimento());
+            cliente.setCidade(cidadeService.PesquisarPorCidadeEstado(cliente.getCidade().getCidade(), cliente.getCidade().getEstado()));
+            this.clienteService.Salvar(cliente);
+            FacesUtils.adicionarMensagem("base_message", GenericExceptionMessageType.INFO, "Cliente gravado com sucesso" );
+        } catch (ServiceException ex) {
+            FacesUtils.adicionarMensagem("base_message", ex, "Ocorreu uma falha ao tentar salvar..");
+        }
+        return "sucesso";
+    }
+
+}
