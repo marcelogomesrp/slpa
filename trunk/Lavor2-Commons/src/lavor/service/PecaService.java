4bd8160fb66ba306de1a39aea5208bdc20815b4a
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2010-03-06 18:37:17 +0000

    Equipamento peca

---
+++ trunk/Lavor2-Commons/src/lavor/service/PecaService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lavor.service;
+
+import lavor.dao.PecaDao;
+
+/**
+ *
+ * @author marcelo
+ */
+public class PecaService {
+    private PecaDao pecaDao;
+
+    public PecaService() {
+    }
+
+    public PecaDao getPecaDao() {
+        return pecaDao;
+    }
+
+    public void setPecaDao(PecaDao pecaDao) {
+        this.pecaDao = pecaDao;
+    }
+
+
+
+
+
+
+
+}
