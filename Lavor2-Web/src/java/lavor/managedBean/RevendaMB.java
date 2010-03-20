06ee0d9a6a6499ae5d2c5024b7e0cdb7ea299eae
marcelogomesrp <marcelogomesrp@f26ce29a-f2e1-11de-82c3-25da7dca7fdf>
2010-03-20 15:49:04 +0000

    Tirei o cascade do commit, isso permitiu salvar e ter o objeto salvo retornado!!! muito bom mesmo

--- Lavor2-Web/src/java/lavor/managedBean/RevendaMB.java
+++ Lavor2-Web/src/java/lavor/managedBean/RevendaMB.java
@@ -5,6 +5,9 @@
 
 package lavor.managedBean;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.model.SelectItem;
 import lavor.entidade.Revenda;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -19,9 +22,11 @@ import org.springframework.stereotype.Controller;
 public class RevendaMB {
 
     private Revenda revenda;
+    private List<SelectItem> revendas;
 
     public RevendaMB() {
         this.revenda = new Revenda();
+        this.revendas = new ArrayList<SelectItem>();
     }
 
     public Revenda getRevenda() {
@@ -30,6 +35,14 @@ public class RevendaMB {
 
     public void setRevenda(Revenda revenda) {
         this.revenda = revenda;
+    }
+
+    public List<SelectItem> getRevendas() {
+        return revendas;
+    }
+
+    public void setRevendas(List<SelectItem> revendas) {
+        this.revendas = revendas;
     }
 
 }
